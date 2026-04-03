00e889fd2011fa9d754209a434a253f10f131943
jolenta <chloe.fling@tutamail.com>
2026-04-02 19:33:15 -0700

    swag and such

---
+++ assets/javascript/sticky-follow.js
@@ -0,0 +1,80 @@
+(function () {
+	var mouseX = 0;
+	var mouseY = 0;
+	var instances = [];
+
+	function tick() {
+		for (var i = 0; i < instances.length; i++) {
+			var s = instances[i];
+			if (!s.following) continue;
+			var el = s.el;
+			var dcx = mouseX - s.anchorCx;
+			var dcy = mouseY - s.anchorCy;
+			dcx = Math.max(-s.maxDx, Math.min(s.maxDx, dcx));
+			dcy = Math.max(-s.maxDy, Math.min(s.maxDy, dcy));
+			s.curTx = dcx;
+			s.curTy = dcy;
+			el.style.transform = "translate(" + s.curTx + "px," + s.curTy + "px)";
+		}
+		requestAnimationFrame(tick);
+	}
+
+	document.addEventListener("mousemove", function (e) {
+		mouseX = e.clientX;
+		mouseY = e.clientY;
+	});
+
+	function attach(el) {
+		var s = {
+			el: el,
+			following: false,
+			anchorCx: 0,
+			anchorCy: 0,
+			maxDx: 0,
+			maxDy: 0,
+			curTx: 0,
+			curTy: 0
+		};
+		instances.push(s);
+
+		el.addEventListener("mouseenter", function (e) {
+			mouseX = e.clientX;
+			mouseY = e.clientY;
+			var r = el.getBoundingClientRect();
+			s.anchorCx = r.left + r.width / 2;
+			s.anchorCy = r.top + r.height / 2;
+			s.maxDx = el.offsetWidth * 0.1;
+			s.maxDy = el.offsetHeight * 0.1;
+			s.curTx = 0;
+			s.curTy = 0;
+			el.style.transform = "translate(0px,0px)";
+			s.following = true;
+		});
+
+		el.addEventListener("mouseleave", function () {
+			s.following = false;
+			s.curTx = 0;
+			s.curTy = 0;
+			el.style.transform = "";
+		});
+	}
+
+	function scan(root) {
+		(root || document).querySelectorAll("[data-sticky-follow]").forEach(function (el) {
+			if (el.dataset.stickyFollowInit) return;
+			el.dataset.stickyFollowInit = "1";
+			attach(el);
+		});
+	}
+
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", function () {
+			scan(document);
+		});
+	} else {
+		scan(document);
+	}
+
+	window.initStickyFollow = scan;
+	requestAnimationFrame(tick);
+})();
