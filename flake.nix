6a648b1e15048d3edc15312e74531f51cd24c1ea
Chloe Fling <chloe.fling@tutamail.com>
2026-07-03 13:05:21 -0700

    initial primitives created

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Vodalus";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs
+              typescript
+            ];
+            shellHook = ''
+              exec fish
+            '';
+          };
+        }
+      );
+    };
+}
