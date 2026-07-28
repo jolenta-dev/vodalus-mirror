b7d2c17d8281a38d4e643e169eb31f5a878993df
Chloe Fling <chloe.fling@tutamail.com>
2026-07-28 13:11:14 -0700

    rough outline of tab implementation

--- src/pages/Admin.ts
+++ src/pages/Admin.ts
@@ -1,9 +1,13 @@
 import { root } from "../assets/primitives/root.js";
 import { PasswordInput } from "../radicals.js";
-import { TextInput, Button, PageTitle } from "../primitives.js";
+import { TextInput, Button, PageTitle, JourneyTabs } from "../primitives.js";
 import { Page } from "./Page.js";
+import { VODALUS_ALICEBLUE, VODALUS_GREEN } from "../assets/theme.js";
 
 export class AdminDashboard extends Page {
+    private title: HTMLDivElement;
+    private inputWrapper: HTMLDivElement;
+
     constructor() {
         super({
             statusMessageText: "why are you here? this is a nonsense page you don't need to know about...",
@@ -13,6 +17,7 @@ export class AdminDashboard extends Page {
         const title: HTMLDivElement = new PageTitle(
             "You sit before the Inire's mirrors and machines, each more wretched than the last. Only the chosen few may enter."
         ).el;
+        this.title = title;
         title.style.textAlign = "center";
         title.style.display = "flex";
         title.style.paddingTop = "20vh";
@@ -21,6 +26,7 @@ export class AdminDashboard extends Page {
 
         const inputWrapper: HTMLDivElement = document.createElement("div");
         inputWrapper.id = "admin-input-wrapper";
+        this.inputWrapper = inputWrapper;
 
         inputWrapper.style.display = "flex";
         inputWrapper.style.flexDirection = "column";
@@ -47,10 +53,76 @@ export class AdminDashboard extends Page {
         enterBtn.style.padding = "1em 2em";
         enterBtn.style.margin = "0.6rem auto 0";
 
+        enterBtn.addEventListener("click", (): void => {
+            this.drawDashboard();
+        });
+
         inputWrapper.appendChild(nameInput);
         inputWrapper.appendChild(passwordInput.el);
         inputWrapper.appendChild(enterBtn);
 
         root().appendChild(inputWrapper);
+    }
+
+    private drawDashboard(): void {
+        root().removeChild(this.title);
+        root().removeChild(this.inputWrapper);
+
+        const title: HTMLDivElement = new PageTitle(
+            "Admin Dashboard"
+        ).el;
+        this.title = title;
+
+        const wrapper: HTMLDivElement = document.createElement("div");
+        this.inputWrapper = wrapper;
+
+        const tabs: JourneyTabs = new JourneyTabs(["Chat", "Status", "SQLite"],
+            (active: HTMLButtonElement): void => {
+                this.changeTab(active.textContent);
+            });
+        tabs.el.style.maxWidth = "90%";
+        tabs.el.style.margin = "auto";
+        wrapper.appendChild(tabs.el);
+
+        const box: HTMLDivElement = document.createElement("div");
+        box.style.width = "100%";
+        box.style.height = "100%";
+        box.style.padding = "10px";
+        box.style.margin = "10px 0";
+        box.style.boxSizing = "border-box";
+        box.style.borderRadius = "15px";
+        box.style.backgroundColor = VODALUS_GREEN;
+        box.style.border = `1px solid ${VODALUS_ALICEBLUE}`;
+
+        wrapper.appendChild(box);
+
+        root().appendChild(wrapper);
+    }
+
+    private changeTab(tab: string): void {
+        switch (tab) {
+            case "Chat":
+                this.drawChatTab();
+                break;
+            case "Status":
+                this.drawStatusTab();
+                break;
+            case "SQLite":
+                this.drawSQLTab();
+                break;
+            default:
+                console.error("invalid tab selection");
+        }
+    }
+
+    // TODO: these need to all be defined
+    private drawChatTab(): void {
+        console.log("chat");
+    }
+
+    private drawStatusTab(): void {
+    }
+
+    private drawSQLTab(): void {
     }
 }
